1eb25bdf090c66fc8e75f8f2ea7aa041ac51e906
Kraachan <87491156+Kraachan@users.noreply.github.com>
2023-04-25 16:16:02 +0200

    fix

--- main.py
+++ main.py
@@ -24,97 +24,105 @@ collection = db["products"]
 
 start_time = time.time()
 
-# Set up the web driver
-options = webdriver.ChromeOptions()
-options.add_argument("--headless")  # Run the driver in headless mode (no GUI)
-driver = webdriver.Chrome(options=options)
+# Array of all the product categories that the program will go through because beautifulsoup breaks if you do over 2200 products on one page 
+urls = ["https://trgovina.mercator.si/market/brskaj#categories=14535405;offset=0",    "https://trgovina.mercator.si/market/brskaj#offset=0;categories=14535446",    "https://trgovina.mercator.si/market/brskaj#offset=0;categories=14535463",    "https://trgovina.mercator.si/market/brskaj#offset=0;categories=14535481",    "https://trgovina.mercator.si/market/brskaj#offset=0;categories=14535512",    "https://trgovina.mercator.si/market/brskaj#offset=0;categories=14535548",    "https://trgovina.mercator.si/market/brskaj#offset=0;categories=14535588",    "https://trgovina.mercator.si/market/brskaj#offset=0;categories=14535612",    "https://trgovina.mercator.si/market/brskaj#offset=0;categories=14535661",    "https://trgovina.mercator.si/market/brskaj#offset=0;categories=14535681",    "https://trgovina.mercator.si/market/brskaj#offset=0;categories=14535711",    "https://trgovina.mercator.si/market/brskaj#offset=0;categories=14535736",    "https://trgovina.mercator.si/market/brskaj#offset=0;categories=14535749",    "https://trgovina.mercator.si/market/brskaj#offset=0;categories=14535768",    "https://trgovina.mercator.si/market/brskaj#offset=0;categories=14535803",    "https://trgovina.mercator.si/market/brskaj#offset=0;categories=14535810",    "https://trgovina.mercator.si/market/brskaj#offset=0;categories=14535837",    "https://trgovina.mercator.si/market/brskaj#offset=0;categories=14535864",    "https://trgovina.mercator.si/market/brskaj#offset=0;categories=14535906",    "https://trgovina.mercator.si/market/brskaj#offset=0;categories=14535941",    "https://trgovina.mercator.si/market/brskaj#offset=0;categories=14535984",    "https://trgovina.mercator.si/market/brskaj#offset=0;categories=14536021",    "https://trgovina.mercator.si/market/brskaj#offset=0;categories=14536058",    "https://trgovina.mercator.si/market/brskaj#offset=0;categories=14536089",    "https://trgovina.mercator.si/market/brskaj#offset=0;categories=16873196"]
 
-# Navigate to the webpage
-url = "https://trgovina.mercator.si/market/brskaj#"
-driver.get(url)
-
-# Wait for the desired elements to load on the page
-wait = WebDriverWait(driver, 10)  # Wait for up to 10 seconds
-wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "box.item.product.rotation.size11")))
-
-# Parse the HTML content of the page using Beautiful Soup
-#print(soup)
-
-product_counter = 0
-product_links = set()
-
-# Loop through the product divs and extract the desired information
-while True:
+total_products = 0
+for url in urls:
+    # Set up the web driver
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless")  
+    driver = webdriver.Chrome(options=options)
+    driver.get(url)
+    wait = WebDriverWait(driver, 10)
+    wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "box.item.product.rotation.size11")))
     soup = BeautifulSoup(driver.page_source, "html.parser")
-    # Find all the product divs on the page
-    product_divs = soup.find_all("div", {"class": "box item product rotation size11"})
-    # If there is no more content to load, break out of the loop
-    if len(product_divs) == 0:
-        break
+    numberOfProducts = int(soup.find("span", {"class": "product-find-counter"}).text.strip()) #Finds the number of products to extract
+    print(f"----------------STARTING TO EXTRACT NEW CATEGORY----------------")
+    print(url)
+    print(numberOfProducts)
+    product_links = set()
+    product_counter = 0
+    # Loop through the product divs and extract the desired information
+    while product_counter <= numberOfProducts:
+        soup = BeautifulSoup(driver.page_source, "html.parser")
+        # Find all the product divs on the page
+        product_divs = soup.find_all("div", {"class": "box item product rotation size11"})
 
-    for product_div in product_divs:
-        # Find the div element with class "product-description"
-        description_div = product_div.find("div", {"class": "product-description"})
-        
-        # Extract the product name from the "product-name" 
-        product_name_link = description_div.find("a", {"class": "lib-product-name"})
-        product_name = product_name_link.text.strip()
-        product_link = "https://trgovina.mercator.si" + product_name_link.get('href')
-        
-        # If we have already seen this product link, skip it
-        if product_link in product_links:
-            print(f"----------------DUPLICATE PRODUCT FOUND SKIPPING----------------")
-            continue
-        
-        product_links.add(product_link)
-        product_counter += 1
-        
-        print("Product name:", product_name)
-        print("Product link:", product_link)
+        if product_counter == numberOfProducts:
+            break
 
-        # Extract the price from the "price" 
-        price = description_div.find("strong", {"class": "lib-product-price"}).text.strip()
-        print("Price:", price)
+        for product_div in product_divs:
+            # Find the div element with class "product-description"
+            description_div = product_div.find("div", {"class": "product-description"})
+            
+            # Extract the product name from the "product-name" 
+            product_name_link = description_div.find("a", {"class": "lib-product-name"})
+            product_name = product_name_link.text.strip()
+            product_link = "https://trgovina.mercator.si" + product_name_link.get('href')
 
-        # Extract the image source from the "product-image" 
-        product_image_link = product_div.find("div", {"class": "default sideA lib-cart"}).find("a", {"class": "product-image"})
-        product_image = product_image_link.find("img").get("src")
-        print("Product image:", product_image)
+            if product_counter == numberOfProducts:
+                break
+            # If we have already seen this product link, skip it
+            if product_link in product_links:
+                if product_counter == numberOfProducts:
+                    break
+                print(f"----------------DUPLICATE PRODUCT FOUND SKIPPING----------------")
+                continue
+            
+            product_counter += 1
+            total_products += 1
 
-        # Create a new document to insert into the MongoDB collection
-        document = {
-            "name": product_name,
-            "image": product_image,
-            "link": product_link,
-            "prices": [
-                {
+            product_links.add(product_link)
+            
+            print("Product name:", product_name)
+            print("Product link:", product_link)
+
+            # Extract the price from the "price" 
+            price = description_div.find("strong", {"class": "lib-product-price"}).text.strip()
+            print("Price:", price)
+
+            # Extract the image source from the "product-image" 
+            product_image_link = product_div.find("div", {"class": "default sideA lib-cart"}).find("a", {"class": "product-image"})
+            product_image = product_image_link.find("img").get("src")
+            print("Product image:", product_image)
+
+            # Create a new document to insert into the MongoDB collection
+            document = {
+                "name": product_name,
+                "image": product_image,
+                "link": product_link,
+                "prices": [
+                    {
+                        "value": price,
+                        "date": datetime.datetime.now()
+                    }
+                ],
+            }
+
+            print(f"----------------INSERTING PRODUCT #{product_counter} INTO DATABASE | TOTAL PRODUCTS: {total_products} | NUMBER OF PRODUCTS: {numberOfProducts}----------------")
+
+            existing_document = collection.find_one({"name": product_name})
+            if existing_document:
+                # If the product exists, append the new price to the "prices" array
+                existing_document["prices"].append({
                     "value": price,
                     "date": datetime.datetime.now()
-                }
-            ],
-        }
+                })
+                # Update the existing document with the new price
+                collection.replace_one({"_id": existing_document["_id"]}, existing_document)
+            else:
+                # If the product does not exist, insert the new document into the collection
+                collection.insert_one(document)
 
-        print(f"----------------INSERTING PRODUCT #{product_counter} INTO DATABASE----------------")
+        if product_counter == numberOfProducts:
+                break
+        
+        # Wait for the new content to load
+        wait.until(EC.presence_of_element_located((By.XPATH, "//div[@class='box item product rotation size11'][last()]")))
 
-
-        existing_document = collection.find_one({"name": product_name})
-        if existing_document:
-            # If the product exists, append the new price to the "prices" array
-            existing_document["prices"].append({
-                "value": price,
-                "date": datetime.datetime.now()
-            })
-            # Update the existing document with the new price
-            collection.replace_one({"_id": existing_document["_id"]}, existing_document)
-        else:
-            # If the product does not exist, insert the new document into the collection
-            collection.insert_one(document)
-
-    # Wait for the new content to load
-    wait.until(EC.presence_of_element_located((By.XPATH, "//div[@class='box item product rotation size11'][last()]")))
-
-    # Scroll down to the bottom of the page
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        # Scroll down to the bottom of the page
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
 # Close the web driver
 driver.quit()
@@ -122,5 +130,5 @@ driver.quit()
 end_time = time.time()
 elapsed_time = end_time - start_time
 
-print(f"Retrieved and inserted {product_counter} products in {elapsed_time:.2f} seconds.")
+print(f"Retrieved and inserted {total_products} products in {elapsed_time:.2f} seconds.")
 
